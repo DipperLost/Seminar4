80d3cdf8a8c338514034ccea824e40c6ffcb4b5f
dipperlost <dipper>
2022-08-20 14:57:21 +0300

    task29

---
+++ task29/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите колличество элементов массива: ");
+int i = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [i];
+Random rnd = new Random();
+for (int count = 0; count < i;)
+{
+    array[count] = Convert.ToInt32(rnd.Next(0, 10));
+    count++;
+}
+for (int count = 0; count < i - 1;)
+{
+    Console.Write(array[count] + ",");
+    count++;
+}
+Console.WriteLine(array[i - 1]);
