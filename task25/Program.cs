9bf2b326ea5c9a624195a5faae5e61b726b34af6
dipperlost <dipper>
2022-08-20 15:46:31 +0300

    task25

---
+++ task25/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+numberB = Math.Abs(numberB);
+int result = numberA;
+for(int count = 0; numberB > 1; count++)
+{
+    result = result * numberA;
+    numberB = numberB - 1;
+}
+Console.WriteLine(result);
